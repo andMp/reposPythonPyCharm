569275262b6c85a168afaec08ff460b7069dabaf
andMp <myposk@gmail.com>
2025-06-07 17:31:19 +0300

    progaZapisSprav

--- main.py
+++ main.py
@@ -1,106 +1,141 @@
-from secrets import choice
 
-vsiKont=[]
-def stvorKont():
-    print("Zapovnit dani nizce:")
-    ima=input("Ima: ")
-    telefon=input("Telefon: ")
-    email=input("Email: ")
-    adresa = input("Adresa: ")
-    kontNS=[]
-    n=int(input("Vvedit k-st kontaktiv pri NS: "))
-    for i in range(n):
-        imaNS = input("Ima: ")
-        telefonNS = input("Telefon: ")
-        emailNS = input("Email: ")
-        adresaNS = input("Adresa: ")
-        kontNS.append({
-            "ima": imaNS,
-            "telefon": telefonNS,
-            "email": emailNS,
-            "adresa": adresaNS
-        })
-    kont={
-        "ima": ima,
-        "telefon": telefon,
-        "email": email,
-        "adresa": adresa,
-        "kontNS": kontNS
-    }
-    vsiKont.append(kont)
-    print("Kontakt dodan!")
+# написать To-Do List с CRUD архитектурой и записем и чтением  с файла)
 
-def pereglad():
-    if not vsiKont:
-        print("Vsi kontaktov")
+robFile = "spisokSprav.txt"
+def zavant():
+    zavd =[]
+    try:
+        with open(robFile,"r",encoding="utf-8") as f:
+            for line in f:
+                line = line.strip()
+                if line:
+                    p = line.split(" ===== ")
+                    if len(p) == 2:
+                        zavd.append({"Zagolovok": p[0],"Poyasnenna": p[1]})
+    except FileNotFoundError:
+        pass
+    return zavd
+
+def zberZavd(zavd):
+    with open(robFile,"w",encoding="utf-8") as f:
+        for zav in zavd:
+            f.write(f"{zav['Zagolovok']} ===== {zav['Poyasnenna']}\n")
+
+def dodZ(zavd):
+    zag = input("Nazva zavd: ")
+    opis = input("Opis zavd: ")
+    zavd.append({"Zagolovok":zag, "Poyasnenna":opis})
+    zberZavd(zavd)
+    print("Zavdanna dodano.")
+
+def pereglad(zavd):
+    print()
+    if not zavd:
+        print("Zavdanna vidsutni!")
     else:
-        for i, kont in enumerate(vsiKont,start=1):
-            print(f"\nKontakt #{i}:")
-            print(f" Ima: {kont['ima']}")
-            print(f" Telefon: {kont['telefon']}")
-            print(f" Email: {kont['email']}")
-            print(f" Adresa: {kont['adresa']}")
-            print(" Kontakti NS:")
-            for j, kNS in enumerate(kont['kontNS'], start=1):
-                print(f" [{j}]Ima: {kNS['ima']}, Telefon: {kNS['telefon']}, Email: {kNS['email']}, Adresa: {kNS['adresa']}")
+        for i, zav in enumerate(zavd, start=1):
+            print(f"{i}. {zav['Zagolovok']} - {zav['Poyasnenna']}")
 
-def redag():
-    pereglad()
+def onovl(zavd):
+    pereglad(zavd)
     try:
-        nK=int(input("Vvedit nomer kontakta dla redaguvanna: "))-1
-        if 0 <= nK <= len(vsiKont):
-            kont=vsiKont[nK]
-            print(f"Ne vvodte dani shob ne zminuvati.")
-            nName=input(f"Nove ima kontakta [{kont['ima']}]: ") or kont['ima']
-            nTel = input(f"Noviy telefon [{kont['telefon']}]: ") or kont['telefon']
-            nEmail= input(f"Noviy email [{kont['email']}]: ") or kont['email']
-            nAddr= input(f"Nova adresa [{kont['adresa']}]: ") or kont['adresa']
-            kont['ima']=nName
-            kont['telefon']=nTel
-            kont['email']=nEmail
-            kont['adresa']=nAddr
-            print(f"Kontakt onovlen!")
+        ind = int(input("Vvedit nomer zavd. dla yogo redaguvanna: ")) - 1
+        if 0<=ind<len(zavd):
+            Zagolovok = input("Nova nazva (shob ne zmin, zaliste poroznim): ")
+            Poyasnenna = input("Opis zavd (shob ne zmin, zaliste poroznim): ")
+            if Zagolovok:
+                zavd[ind]["Zagolovok"] = Zagolovok
+            if Poyasnenna:
+                zavd[ind]["Poyasnenna"] = Poyasnenna
+            zberZavd(zavd)
+            print("Zavdanna onovleno.")
         else:
-            print("Nevirniy nomer kontakta.")
+            print("Nevirniy nomer.")
     except ValueError:
-        print("Vvedit nomer kontakta.")
-def vidal():
-    pereglad()
+        print("Vvedit virniy nomer.")
+
+def vidal(zavd):
+    pereglad(zavd)
     try:
-        nk = int(input("Vvedit nomer kontakta dla vidalenna: "))
-        if 0 <= nk <= len(vsiKont):
-            vsiKont.pop(nk)
-            print(f"Kontakt vidalen!")
+        ind = int(input("Vvedit nomer zavd. dla yogo vidalenna: ")) - 1
+        if 0<=ind<len(zavd):
+            vid = zavd.pop(ind)
+            zberZavd(zavd)
+            print(f"Zavdanna '{vid['Zagolovok']}' vidaleno.")
         else:
-            print("Nevirniy nomer kontakta.")
+            print("Nevirniy nomer.")
     except ValueError:
-        print("Vvedit nomer kontakta.")
+        print("Vvedit virniy nomer.")
 
 def main():
+    zavd = zavant()
     while True:
-        print("\n=== Menu kontaktiv ===")
-        print("1.Stvoriti kontakt")
-        print("2.Pereglanuti vsi kontakti")
-        print("3.Redaguvati kontakt")
-        print("4.Vidaliti kontakt")
-        print("5.Zaversiti robotu.")
-        c = input("Zrobit vas vibir: ")
-        if c == '1':
-            stvorKont()
-        elif c == '2':
-            pereglad()
-        elif c == '3':
-            redag()
-        elif c == '4':
-            vidal()
-        elif c == '5':
-            print("Zaversenna roboti!")
-            break
-        else:
-            print("Ne virniy variant! Sprobuy znov!")
+        print("\n=== Menu roboti z zavd: ===")
+        print("1. Pereglanuti vsi zavd")
+        print("2. Dodati zavd")
+        print("3. Redaguvati zavd")
+        print("4. Vidaliti zavd")
+        print("5. Exit")
+        vib = input("Oberit bazanu diu: ")
+
+        match vib:
+            case "1":
+                pereglad(zavd)
+            case "2":
+                dodZ(zavd)
+            case "3":
+                onovl(zavd)
+            case "4":
+                vidal(zavd)
+            case "5":
+                print("Do pobachenna!")
+                break
+            case _:
+                print("Nevirniy nomer.")
+
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+# Написать консольное приложение для управления библиотекой. CRUD, регистрировать читателей,
+# оформлять и возвращать книги и просматривать статистику
+# (кто взял, кто вернул, кто не вернул, какие книги есть в наличии в каком количестве)
+# class Chitach:
+#     def __init__(self, name):
+#         self.name = name
+#         self.spis = []
+#
+# class Kniga:
+#     def __init__(self, nazva):
+#         self.nazva = nazva
+#         self.nayavn = 'nemaE'
+#
+# class Bibka:
+#     def __init__(self):
+#         self.__spKnig = []
+#         self.__spChit = []
+#     def regCh(self,name):
+#         self.__spChit.append(Chitach(name))
+#     def reestrKn(self, nazva):
+#         self.__spKnig.append(Kniga(nazva))
+#     def vidKn(self, nazvaKn, priz):
+#         try:
+#             if self.__spKnig.count(Kniga.nazva===nazvaKn):
+#                 if :
+#
+#             else:
+#                 print(f"Kniga {nazvaKn} vidsutna в нашій бібліотеці.")
+#         except:
+#             print("Якась помилка, якщо цікаво розбирайся.")
+
+
+
+
+
 
 
 
