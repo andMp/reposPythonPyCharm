5327b1e9c3d05c85ecc7d30862b44e85344fda30
andMp <myposk@gmail.com>
2025-06-12 00:00:02 +0300

    bibliotekuZrobiv!

--- main.py
+++ main.py
@@ -104,17 +104,18 @@
 # Написать консольное приложение для управления библиотекой. CRUD, регистрировать читателей,
 # оформлять и возвращать книги и просматривать статистику
 # (кто взял, кто вернул, кто не вернул, какие книги есть в наличии в каком количестве)
+
 class Chitach:
-    def __init__(self, name, id):
+    def __init__(self, name, idCh):
         self.name = name
-        self.nomAbon = id
+        self.nomAbon = idCh
         self.spisKn = []
 
 class Kniga:
-    def __init__(self, nazva, id):
+    def __init__(self, nazva, idKn):
         self.nazva = nazva
-        self.id = id
-        self.nayavn = 0
+        self.idKn = idKn
+        self.vidanaKomu = 0
 
 class Bibka:
     def __init__(self):
@@ -131,7 +132,7 @@ class Bibka:
             newChitach = Chitach(name, self.__nomNewAb)
             self.__spChit.append(newChitach)
             self.__nomNewAb += 1
-            print(f'Vi dodali novogo vidviduvacha biblioteki za imam "{newChitach.name}", yogo abon.nomer {newChitach.nomAbon}.')
+            print(f'Vi dodali novogo vidviduvacha biblioteki za imyam "{newChitach.name}", yogo abon.nomer {newChitach.nomAbon}.')
 
     def reestrKn(self):
         nazva = input("Vvedit nazvu knigi: ").strip()
@@ -141,81 +142,210 @@ class Bibka:
             newKniga = Kniga(nazva, self.__nomNewKn)
             self.__spKnig.append(newKniga)
             self.__nomNewKn += 1
-            print(f'Vi dodali knigu pid nazvou "{newKniga.nazva}" do biblioteki, ii nomer {newKniga.id}.')
+            print(f'Vi dodali knigu pid nazvou "{newKniga.nazva}" do biblioteki, ii nomer {newKniga.idKn}.')
 
-    def vidatiKn(self):
+    def povernORvidati(self,dia):
         try:
-            while True:
-                idAb = input("Vvedit id abonenta: ")
-                ab = [ab for ab in self.__spChit if ab.self.nomAbon == idAb]
+                idAb = int(input("Vvedit id abonenta: "))
+                ab = next((a for a in self.__spChit if a.nomAbon == idAb), None)
                 if not ab:
-                    print(f'Abonement z nomerom {idAb} visutniy.')
+                    print(f'Abonement z nomerom {idAb} vidsutniy.')
                 else:
-                    idKn = input("Vvedit id knigi: ")
-                    kn = [kn for kn in self.__spKnig if kn.self.id == idKn]
+                    idKn = int(input("Vvedit id knigi: "))
+                    kn = next((kn for kn in self.__spKnig if kn.idKn == idKn), None)
                     if not kn:
-                        print(f'Kniga z id {idKn} visutna.')
+                        print(f'Kniga z id {idKn} vidsutna.')
+                        return
+                    if dia == 'vidati' and kn.vidanaKomu != 0:
+                        print(f'Kniga z id {idKn} uzhe vydana.')
+                        return
                     else:
-                        ab.spisKn.append(kn.self.id)
-                        kn.self.nayavn = ab.self.nomAbon
-                        print(f'Kniga "{kn.self.nazva}" z id={kn.self.id} vidana abonentu {ab.self.name} z id={ab.self.nomAbon}.')
-        except:
-            print("Pomilka!")
+                        if dia=='povern':
+                            if idKn in ab.spisKn:
+                                ab.spisKn.remove(idKn)
+                                kn.vidanaKomu = 0
+                                print(f'Kniga "{kn.nazva}" z id={kn.idKn} povernuv abonent {ab.name} z id={ab.nomAbon}.')
+                            else:
+                                print("Abonent ne mae ciei knigi.")
+                        elif dia=='vidati':
+                            ab.spisKn.append(kn.idKn)
+                            kn.vidanaKomu = ab.nomAbon
+                            print(f'Kniga "{kn.nazva}" z id={kn.idKn} vidana abonentu {ab.name} z id={ab.nomAbon}.')
 
-    def povernKn(self):
-        try:
-            while True:
-                idAb = input("Vvedit id abonenta: ")
-                ab = [ab for ab in self.__spChit if ab.self.nomAbon == idAb]
-                if not ab:
-                    print(f'Abonement z nomerom {idAb} visutniy.')
-                else:
-                    idKn = input("Vvedit id knigi: ")
-                    kn = [kn for kn in self.__spKnig if kn.self.id == idKn]
-                    if not kn:
-                        print(f'Kniga z id {idKn} visutna.')
-                    else:
-                        ab.spisKn.remove(idKn)
-                        kn.self.nayavn = 0
-                        print(f'Kniga "{kn.self.nazva}" z id={kn.self.id} povernuv abonent {ab.self.name} z id={ab.self.nomAbon}.')
-        except:
-            print("Pomilka!")
+        except Exception as e:
+            print(f"Pomilka: {e}")
 
     def stat(self):
         while True:
-            print("\nZagalna statistika:")
-            print(f'- zagalna kilkist abonentiv: {len.self.__spChit}')
-            print(f'- zagalna kilkist abonentiv: {len.self.__spKnig}')
-            print(f'- kilkist vidanih knig: {len.list(filter(lambda k: k.nayavn != 0, self.__spKnig))}')
-            print(f'- kilkist knig v nayavn: {len.list(filter(lambda k: k.nayavn == 0, self.__spKnig))}')
-
-
                 print("\nVarianti statistiki:")
                 print("1. Pereglanuti zagalnu statistiku")
                 print("2. Abonenti z knigami")
-                print("3. Redaguvati zavd")
-                print("4. Vidaliti zavd")
-                print("5. Exit")
-                vib = input("Oberit bazanu diu: ")
+                print("3. Exit")
 
+                vib = input("Oberit bazanu diu: ")
                 match vib:
                     case "1":
-                        pereglad(zavd)
+                        print("\nZagalna statistika:")
+                        print(f'- zagalna kilkist abonentiv: {len(self.__spChit)}')
+                        print(f'- zagalna kilkist knig: {len(self.__spKnig)}')
+                        print(f'- kilkist vidanih knig: {len(list(filter(lambda k: k.vidanaKomu != 0, self.__spKnig)))}')
+                        print(f'- kilkist knig v nayavn: {len(list(filter(lambda k: k.vidanaKomu == 0, self.__spKnig)))}')
                     case "2":
-                        dodZ(zavd)
+                        print("\nAbonenti z knigami:")
+                        spAb = [ab for ab in self.__spChit if len(ab.spisKn) > 0]
+                        for i, ab in enumerate(spAb, start=1):
+                            print(f"{i}. {ab.name}. Knigi: {', '.join(kn.nazva for kn in self.__spKnig if kn.idKn in ab.spisKn)}.")
                     case "3":
-                        onovl(zavd)
-                    case "4":
-                        vidal(zavd)
-                    case "5":
                         print("Do pobachenna!")
                         break
                     case _:
                         print("Nevirniy nomer.")
 
+def main():
+    bib = Bibka()
+    while True:
+        print("\n=== Menu biblioteki: ===")
+        print("1. Zareestruvati chitacha")
+        print("2. Zareestruvati knigu")
+        print("3. Vidati knigu")
+        print("4. Povernenna knigi")
+        print("5. Statistika")
+        print("6. Exit")
+
+        vib = input("Oberit bazanu diu: ").strip()
+        match vib:
+            case "1":
+                bib.regCh()
+            case "2":
+                bib.reestrKn()
+            case "3":
+                bib.povernORvidati('vidati')
+            case "4":
+                bib.povernORvidati('povern')
+            case "5":
+                bib.stat()
+            case "6":
+                print("Do pobachenna!")
+                break
+            case _:
+                print("Nevirniy nomer.")
 
 if __name__ == '__main__':
     main()
+
+# import random
+# import time
+# import os
+# import msvcrt
+#
+# WIDTH = 20
+# HEIGHT = 10
+#
+# class Kulka:
+#     def __init__(self, x):
+#         self.x = x
+#         self.y = 0
+#
+#     def Padinna(self):
+#         self.y += 1
+#
+# class Platforma:
+#     def __init__(self):
+#         self.x = WIDTH // 2
+#         self.width = 7
+#
+#     def vLivo(self):
+#         if self.x > 0:
+#             self.x -= 1
+#
+#     def vPravo(self):
+#         if self.x + self.width < WIDTH:
+#             self.x += 1
+#
+#     def Roztash(self, x):
+#         return self.x <= x < self.x + self.width
+#
+# class Gra:
+#     def __init__(self):
+#         self.platform = Platforma()
+#         self.kulki = []
+#         self.rahunok = 0
+#         self.sproba = 3
+#         self.shvidk = 0.4
+#
+#     def stvorKulki(self):
+#         x = random.randint(0, WIDTH - 1)
+#         self.kulki.append(Kulka(x))
+#
+#     def onovl(self):
+#         os.system("cls" if os.name == "nt" else "clear")
+#
+#         pole = [[" " for _ in range(WIDTH)] for _ in range(HEIGHT)]
+#
+#         for kulka in self.kulki:
+#             if 0 <= kulka.y < HEIGHT:
+#                 pole[kulka.y][kulka.x] = "0"
+#
+#         for i in range(self.platform.width):
+#             px = self.platform.x + i
+#             pole[HEIGHT - 1][px] = "="
+#
+#         for row in pole:
+#             print("".join(row))
+#         print(f"\nRahunok: {self.rahunok}   Sprob: {self.sproba}")
+#
+#     def logika(self):
+#         for kulka in self.kulki:
+#             kulka.Padinna()
+#
+#         for kulka in self.kulki[:]:
+#             if kulka.y == HEIGHT - 1:
+#                 if self.platform.Roztash(kulka.x):
+#                     self.rahunok += 1
+#                 else:
+#                     self.sproba -= 1
+#                 self.kulki.remove(kulka)
+#
+#     def get_char(self):
+#         if msvcrt.kbhit():
+#             return msvcrt.getch().decode('utf-8').lower()
+#         return ""
+#
+#     def proces(self):
+#         try:
+#             while self.sproba > 0:
+#                 if random.random() < 0.3:
+#                     self.stvorKulki()
+#
+#                 self.onovl()
+#                 hid = self.get_char()
+#
+#                 platform_speed = 3 if hid in ("a", "d") else 1
+#
+#                 if hid == "a":
+#                     for _ in range(platform_speed):
+#                         self.platform.vLivo()
+#                 elif hid == "d":
+#                     for _ in range(platform_speed):
+#                         self.platform.vPravo()
+#
+#                 self.logika()
+#
+#                 self.shvidk = max(0.1, 0.4 - self.rahunok * 0.01)
+#                 time.sleep(self.shvidk)
+#
+#             self.onovl()
+#             print("\nKinec gri!")
+#             print(f"Vi zibrali: {self.rahunok}")
+#         except KeyboardInterrupt:
+#             print("\nVihid z gri.")
+#
+#
+# if __name__ == "__main__":
+#     game = Gra()
+#     game.proces()
+
+
 
 
 
