db1851db331a573e964a91881d2fc6b5b33209b4
andMp <myposk@gmail.com>
2025-06-16 23:41:42 +0300

    biblioteka_MVC_vikonav!(dodavFile)

---
+++ repositories/__init__.py
@@ -0,0 +1,5 @@
+# repositories/__init__.py
+from .kniga_repository import KnigaRepository
+from .chitach_repository import KoristuvacRepository
+
+# from repositories import KnigaRepository // Тепер можна імпортувати так
