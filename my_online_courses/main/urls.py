0336590f36ca9688632c52ef6f2113293ffb1918
andMp <myposk@gmail.com>
2025-07-06 21:40:39 +0300

    zmini

--- my_online_courses/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('course/<int:pk>/', views.course_detail, name='course_detail'),
-]
