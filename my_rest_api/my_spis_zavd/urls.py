7128520d1952ad2e5624d9cea7e09c9a03959a18
andMp <myposk@gmail.com>
2025-07-08 15:49:34 +0300

    spis_zavd_vikonav!

--- my_rest_api/my_spis_zavd/urls.py
+++ my_rest_api/my_spis_zavd/urls.py
@@ -1,3 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from my_rest_api. import
+from .views import ZavdViewSet, index
+
+router=DefaultRouter()
+router.register(r'my_spis_zavd',ZavdViewSet)
+
+urlpatterns=[
+    path('',index),
+    path('api/',include(router.urls)),
+]
